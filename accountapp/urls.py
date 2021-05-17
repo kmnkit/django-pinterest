78930bb2492462ef19e4754d50eeb6ed31c4e152
Marco Kang <kmnkit@gmail.com>
2021-05-18 00:07:19 +0900

    until delete user

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import AccountCreateView, hello_world
+from .views import (
+    AccountCreateView,
+    AccountDetailView,
+    AccountUpdateView,
+    AccountDeleteView,
+    hello_world,
+)
 
 app_name = "accountapp"
 
@@ -11,4 +17,7 @@ urlpatterns = [
     ),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("create/", AccountCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", AccountUpdateView.as_view(), name="update"),
+    path("detail/<int:pk>/", AccountDetailView.as_view(), name="detail"),
+    path("delete/<int:pk>/", AccountDeleteView.as_view(), name="delete"),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
-
-# from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.shortcuts import render
-
 from django.urls import reverse_lazy
+from .forms import AccountUpdateForm
 
 User = get_user_model()
 
@@ -26,3 +24,22 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = "accountapp/create.html"
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = "target_user"
+    template_name = "accountapp/detail.html"
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    context_object_name = "target_user"
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/update.html"
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+    template_name = "accountapp/delete.html"
