e1fda98a9a1f336c5e840d3608c79ead4b16e15e
Marco Kang <kmnkit@gmail.com>
2021-05-18 12:43:16 +0900

    profile update

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,7 +1,8 @@
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 from django.urls import path
 
 app_name = "profileapp"
 urlpatterns = [
     path("create/", ProfileCreateView.as_view(), name="create"),
+    path("update/<int:pk>", ProfileUpdateView.as_view(), name="update"),
 ]
