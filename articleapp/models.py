a452ba9905ae0db6d9c2aa54c8a5bc51568dee9e
Marco Kang <kmnkit@gmail.com>
2021-05-18 14:13:16 +0900

    Articles

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Article(models.Model):
+    writer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="articles", null=True
+    )
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to="articles/", null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, auto_created=True)
--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import (
+    ArticleCreationView,
+    ArticleDetailView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
 
 
 app_name = "articleapp"
@@ -7,5 +13,25 @@ app_name = "articleapp"
 urlpatterns = [
     path(
         "list/", TemplateView.as_view(template_name="articleapp/list.html"), name="list"
+    ),
+    path(
+        "create/",
+        ArticleCreationView.as_view(template_name="articleapp/create.html"),
+        name="create",
+    ),
+    path(
+        "detail/<int:pk>",
+        ArticleDetailView.as_view(template_name="articleapp/detail.html"),
+        name="detail",
+    ),
+    path(
+        "update/<int:pk>",
+        ArticleUpdateView.as_view(template_name="articleapp/update.html"),
+        name="update",
+    ),
+    path(
+        "delete/<int:pk>",
+        ArticleDeleteView.as_view(template_name="articleapp/delete.html"),
+        name="delete",
     ),
 ]
