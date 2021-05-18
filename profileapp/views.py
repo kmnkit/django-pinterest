4a49ca0e69e6c506a34a3b6cf889373c9e2536b9
Marco Kang <kmnkit@gmail.com>
2021-05-18 13:06:35 +0900

    profile end

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from profileapp.decorators import profile_ownership_required
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 from .forms import ProfileCreationForm
@@ -10,7 +10,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy("accountapp:hello_world")
     template_name = "profileapp/create.html"
 
     def form_valid(self, form):
@@ -18,6 +17,9 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})
 
 
 @method_decorator(profile_ownership_required, "get")
